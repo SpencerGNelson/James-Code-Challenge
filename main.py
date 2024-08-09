a589d73c892ba364759b69c7b123fbb835b00310
SpencerGNelson <138039144+SpencerGNelson@users.noreply.github.com>
2024-08-09 14:07:05 -0600

    Made adjustments to time_overlap function to not need a counter

--- main.py
+++ main.py
@@ -8,15 +8,11 @@ class Timeframe:
 def time_overlap(time_1, time_2):
     if time_1.end_time < time_2.start_time:
         raise ValueError('No time conflict')
-    else:
-        overlap_start = time_1.end_time - time_2.start_time + 10 #add 10 to remain in military time
-        counter = overlap_start
-        while counter < time_1.end_time:
-            counter += 1
-    return(overlap_start, counter)
+        
+    return(time_2.start_time, time_1.end_time)
 
 def main():
-    time1 = Timeframe(12, 15)
+    time1 = Timeframe(12, 16)
     time2 = Timeframe(14, 16)
 
     print(time_overlap(time1, time2))
